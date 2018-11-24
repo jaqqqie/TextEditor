0b0981f1ee5f9b0c6b3820f4f43c8547a2605333
juderIV <juder.stidham@gmail.com>
2018-11-24 13:58:10 -0600

    Added functionality to save state on application closing
    Used the Gson library to save the state of the editor when the user
    closed the application. Upon re-opening, the editor would parse the
    .json file and retrieve the size of the editor window, along with the
    text.

--- Java_TextEditor/src/java_texteditor/Java_TextEditor.java
+++ Java_TextEditor/src/java_texteditor/Java_TextEditor.java
@@ -1,25 +1,81 @@
 package java_texteditor;
 
-import javafx.application.Application;;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Java_TextEditor extends Application {
-    
+    private int width = 300;
+    private int height = 250;
+    private String text = "Type here....";
+    public static Settings settings;
     @Override
     public void start(Stage primaryStage) {
-        //creates a new text area for the user
-        TextArea text = new TextArea();
-        //adding a prompt to the inital text box
-        text.setPromptText("Type here....");
+        File file = new File("data.json");
+        TextArea textArea = new TextArea();
+        Scene scene;
+        //creates scene based on previously closed file 
+        //ELSE creates a new scene with predefined settings
+        if(file.exists()){
+            create(file);
+            textArea.setText(settings.getText());
+            scene = new Scene(textArea, 
+                    settings.getWidth(), settings.getHeight());
+        }else{
+            textArea.setPromptText(text);
+            scene = new Scene(textArea, width, height);
+        }
         
-        Scene scene = new Scene(text, 300, 250);
-        //setting the title of the textarea
-        primaryStage.setTitle("Text Editor");
+        primaryStage.setTitle("GUI Lab 5");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+            //handles when the window is closed
+            @Override
+            public void handle(WindowEvent e){
+                //takes the settings of the closed window and
+                //adds to settings class
+                settings = new Settings();
+                settings.setHeight(scene.getHeight());
+                settings.setWidth(scene.getWidth());
+                settings.setText(textArea.getText());
+                GsonBuilder builder = new GsonBuilder();
+                //created to change java code to json
+                Gson gson = builder.create();
+                try{
+                    //takes on json code and writes it to file 
+                    FileWriter writer = new FileWriter("data.json");
+                    String input = gson.toJson(settings);
+                    writer.write(input);
+                    writer.close();
+                }catch(IOException ex){
+                    System.out.println(ex);
+                }
+            }
+        });
     }
+    public static void create(File file){
+           try{
+               FileReader fr = new FileReader(file);
+               BufferedReader br = new BufferedReader(fr);
+               Gson gson = new Gson();
+               settings = gson.fromJson(br.readLine(), Settings.class);
+            }catch(IOException e){
+                System.out.println(e);
+           } 
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
---
+++ Java_TextEditor/src/java_texteditor/Settings.java
@@ -0,0 +1,32 @@
+package java_texteditor;
+
+public class Settings{
+    private double height;
+    private double width;
+    private String text;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
